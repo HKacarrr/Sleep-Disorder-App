3ba35231d24b8f0c73de4160f01e189aad0a595a
Hasan Kacar <hasan@fybu.de>
2025-12-10 21:16:05 +0300

    App main apge added to system with Request hierarchie

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,23 @@
 
 namespace App\Controller;
 
-class MainController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
+class MainController extends AbstractController
+{
+    #[Route('', name: 'app_main', methods: ['GET'])]
+    public function main(): Response
+    {
+        return $this->render('main_page.html.twig');
+    }
+
+    #[Route('/sleep-analyze', name: 'sleep_analyze', methods: ['POST'])]
+    public function sleepAnalyze(Request $request)
+    {
+        sleep(10);
+        dd($request->request->all());
+    }
 }
